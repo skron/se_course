e9a4b3c9b206026e28d402cae7cf4edb61ea007d
skron <skronovo@gmail.com>
2015-02-03 23:42:01 +0100

    Email name update

--- course/app/mailers/subscriber_mailer.rb
+++ course/app/mailers/subscriber_mailer.rb
@@ -1,6 +1,5 @@
 class SubscriberMailer < ActionMailer::Base
-  default from: "info@secourse.cz"
-
+  
   def registration_confirmation(subscriber)
     @subscriber = subscriber
     mail(:to => subscriber.email, :subject => "Potvrzení registrace")
